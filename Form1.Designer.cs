30014465977e8e83b7fd1e2efc3470fb32c6c4cf
Gabriel Henrique <gabrielsala.eng@hotmail.com>
2024-04-27 16:39:44 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Forms1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnAceitar = new Button();
+            btnRejeitar = new Button();
+            txtMensagem = new TextBox();
+            SuspendLayout();
+            // 
+            // btnAceitar
+            // 
+            btnAceitar.BackColor = Color.Lime;
+            btnAceitar.Cursor = Cursors.Hand;
+            btnAceitar.Font = new Font("Papyrus", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            btnAceitar.Location = new Point(379, 146);
+            btnAceitar.Name = "btnAceitar";
+            btnAceitar.Size = new Size(253, 221);
+            btnAceitar.TabIndex = 0;
+            btnAceitar.Text = "SIM";
+            btnAceitar.UseVisualStyleBackColor = false;
+            btnAceitar.BackColorChanged += Aceitar_Click;
+            btnAceitar.Click += Aceitar_Click;
+            // 
+            // btnRejeitar
+            // 
+            btnRejeitar.BackColor = Color.Red;
+            btnRejeitar.Cursor = Cursors.Hand;
+            btnRejeitar.FlatStyle = FlatStyle.Popup;
+            btnRejeitar.Font = new Font("Sans Serif Collection", 8.999999F, FontStyle.Regular, GraphicsUnit.Point);
+            btnRejeitar.ForeColor = Color.Black;
+            btnRejeitar.Location = new Point(68, 146);
+            btnRejeitar.Name = "btnRejeitar";
+            btnRejeitar.Size = new Size(253, 221);
+            btnRejeitar.TabIndex = 1;
+            btnRejeitar.Text = "NÃO";
+            btnRejeitar.UseVisualStyleBackColor = false;
+            btnRejeitar.Click += Rejeitar_Click;
+            // 
+            // txtMensagem
+            // 
+            txtMensagem.Font = new Font("Papyrus", 14F, FontStyle.Bold, GraphicsUnit.Point);
+            txtMensagem.Location = new Point(77, 49);
+            txtMensagem.Name = "txtMensagem";
+            txtMensagem.Size = new Size(573, 52);
+            txtMensagem.TabIndex = 2;
+            txtMensagem.Text = "Cancelar assinatura?";
+            txtMensagem.TextAlign = HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtMensagem);
+            Controls.Add(btnRejeitar);
+            Controls.Add(btnAceitar);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnAceitar;
+        private Button btnRejeitar;
+        private TextBox txtMensagem;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿namespace Forms1
+{
+    public partial class Form1 : Form
+    {
+        int clicks = 0;
+        string[] frases =
+        {
+            "Tem Certeza?",
+            "Are you sure about that??",
+            "¿estas cierto deso?",
+            "rnон уверен?? "
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Aceitar_Click(object sender, EventArgs e)
+        {
+            clicks++;
+
+            txtMensagem.Text = frases[clicks - 1];
+            btnAceitar.Size = new System.Drawing.Size((150 - clicks * 20), (100 - clicks * 20));
+            if (clicks == 1)
+            {
+                BackColor = Color.Red;
+               
+            }
+
+            //txtMensagem.Text = frases[0];
+            //if (clicks == 2)
+            //    txtMensagem.Text = frases[1];
+            //if (clicks == 3)
+            //    txtMensagem.Text = frases[2];
+
+            if (clicks > 3)
+                clicks = 0;
+
+        }
+
+        private void Rejeitar(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Rejeitar_Click(object sender, EventArgs e)
+        {
+            txtMensagem.Text = "Obrigado!";
+            clicks = 0;
+        }
+    }
+}
